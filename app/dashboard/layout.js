f337e0ac0fefef720526f90f5f4910f78a0fad16
harshkeshri65 <24155535@kiit.ac.in>
2026-02-26 23:01:18 +0530

    Update layout.js

--- app/dashboard/layout.js
+++ app/dashboard/layout.js
@@ -4,8 +4,31 @@ export default function DashboardLayout({ children }) {
 
       {/* Sidebar */}
       <div className="sidebar">
-        <h2>Alok Harsh</h2>
-        <p className="subtitle">Rice Mill Billing</p>
+     <div style={{ display: "flex", alignItems: "center", gap: "12px", marginBottom: "10px" }}>
+  
+  <div style={{
+    width: "42px",
+    height: "42px",
+    background: "#facc15",
+    borderRadius: "10px",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    fontWeight: "bold",
+    color: "#111827",
+    fontSize: "18px"
+  }}>
+    AH
+  </div>
+
+  <div>
+    <h2 style={{ margin: 0 }}>Alok Harsh</h2>
+    <p style={{ fontSize: "12px", opacity: 0.6, margin: 0 }}>
+      Rice Mill Billing
+    </p>
+  </div>
+
+</div>
 
         <hr className="divider" />
 
