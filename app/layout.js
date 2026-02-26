33349cfb7821ff606a2f3b6e4b44609ab019c136
harshkeshri65 <24155535@kiit.ac.in>
2026-02-26 19:39:18 +0530

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,14 @@
+export const metadata = {
+  title: "Alok Harsh Rice Mill",
+  description: "Billing System",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
