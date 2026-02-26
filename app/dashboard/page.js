e2ca2fc10a9f7929ec1420baa00c38ed8c572d12
harshkeshri65 <24155535@kiit.ac.in>
2026-02-26 20:38:46 +0530

    Update page.js

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,8 +1,28 @@
+'use client'
+
+import { useEffect } from 'react'
+import { useRouter } from 'next/navigation'
+import { supabase } from '../../lib/supabase'
+
 export default function Dashboard() {
+  const router = useRouter()
+
+  useEffect(() => {
+    const checkUser = async () => {
+      const { data } = await supabase.auth.getSession()
+
+      if (!data.session) {
+        router.push('/login')
+      }
+    }
+
+    checkUser()
+  }, [])
+
   return (
     <div style={{padding:'40px'}}>
       <h1>Welcome to Alok Harsh Rice Mill Dashboard</h1>
-      <p>Login successful ✅</p>
+      <p>Secure area 🔒</p>
     </div>
   )
 }
